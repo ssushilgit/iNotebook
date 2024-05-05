3c2d281524996fe2887cde6f04fef0541c1059c6
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-05 17:01:45 +0545

    Day 3 - Data Validation using express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator'); //import
 
 
 
 // create a User using: POST "/api/auth". Doesnot require Auth
-router.post('/', (req,res)=>{
-    console.log(req.body);
+router.post('/', [
+    body('name','Enter a valid name').isLength({ min: 5 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password', 'Enter a valid password which must be five characters').isLength({ min: 5 })
 
-    const user = User(req.body);
-    user.save();
+], (req, res) => {
+     // Finds the validation errors in this request and wraps them in an object with handy functions
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+       return res.status(400).json({ errors: errors.array() });
+     }
 
-    res.send(req.body)
+     User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+
+      }).then(user => res.json(user)).
+      catch(err=>{console.log(err)
+        res.json({error:'Please enter a unique value for email', message:err.message})
+      })   //validation
+
+    // res.send(req.body)
 })
 
 module.exports = router
