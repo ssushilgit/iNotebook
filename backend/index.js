771ae41f6dc2a35a9d070ccac1e517623a3398fe
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-06 16:24:35 +0545

    Day 4 - Creating ThunderCliend Collections to Manage Requests

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@
 
  connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
 
 app.use(express.json()) //middleware required to use request.body
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,30 +5,40 @@ const { body, validationResult } = require('express-validator'); //import
 
 
 
-// create a User using: POST "/api/auth". Doesnot require Auth
-router.post('/', [
-    body('name','Enter a valid name').isLength({ min: 5 }),
-    body('email','Enter a valid email').isEmail(),
-    body('password', 'Enter a valid password which must be five characters').isLength({ min: 5 })
+// create a User using: POST "/api/auth/createuser". Doesnot require Auth //No login required
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 5 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Enter a valid password which must be five characters').isLength({ min: 5 })
 
-], (req, res) => {
-     // Finds the validation errors in this request and wraps them in an object with handy functions
-     const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-       return res.status(400).json({ errors: errors.array() });
-     }
+], async (req, res) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  // if there ar errors, then return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-     User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
+  //  check whether user with this email exists already
+  try {
+    let user = await User.findOne({ email: req.body.email })
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+    // create a new user
+    user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+    res.json(user)
+  }
+  // catch errors
+  catch (error) {
+    console.error(error.message)
+    res.status(500).send("Some error occured");
+  }
 
-      }).then(user => res.json(user)).
-      catch(err=>{console.log(err)
-        res.json({error:'Please enter a unique value for email', message:err.message})
-      })   //validation
-
-    // res.send(req.body)
 })
 
 module.exports = router
