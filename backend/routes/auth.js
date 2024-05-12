cb27d184414f9e06556bc281b9ba89ef97a07083
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-12 12:25:30 +0545

    Day 7 - Creating a middleware to retrive user info using auth-tokn from JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,12 @@ const User = require('../models/User')
 const { body, validationResult } = require('express-validator'); //import
 const bcrypt = require('bcrypt'); // bcryptjs
 var jwt = require('jsonwebtoken'); // jwt nodejs
+var fetchuser = require('../middleware/fetchuser'); // jwt nodejs
 
-JWT_SECRET = "Sushil is a good boy"
-// create a User using: POST "/api/auth/createuser". Doesnot require Auth //No login required
+
+
+JWT_SECRET = "Sushilisagoodbd$boy"
+// ROUTE 1: create a User using: POST "/api/auth/createuser". Doesnot require Auth //No login required
 // user endpoint
 router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 5 }),
@@ -26,9 +29,9 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exists" })
     }
-    const salt =await bcrypt.genSalt(10); //wait until the promise are resolved, take the value and go the next line,  if not resolved then it will directly go to the next line
-    const secPass =await bcrypt.hash(req.body.password, salt) //await returns promise
-    
+    const salt = await bcrypt.genSalt(10); //wait until the promise are resolved, take the value and go the next line,  if not resolved then it will directly go to the next line
+    const secPass = await bcrypt.hash(req.body.password, salt) //await returns promise
+
     // create a new user
     user = await User.create({
       name: req.body.name,
@@ -37,13 +40,13 @@ router.post('/createuser', [
     })
 
     // object data created
-    const data={
-      user:{
+    const data = {
+      user: {
         id: user.id
       }
     }
-    const authtoken = jwt.sign(data, JWT_SECRET ); // jwt io
-    res.json({authtoken})
+    const authtoken = jwt.sign(data, JWT_SECRET); // jwt io
+    res.json({ authtoken })
   }
   // catch errors
   catch (error) {
@@ -53,48 +56,60 @@ router.post('/createuser', [
 
 })
 
-// Authenticating a User Login using: POST "/api/auth/login". Doesnot require Auth //No login required
+// ROUTE 2: Authenticating a User Login using: POST "/api/auth/login". Doesnot require Auth //No login required
 // user endpoint
 router.post('/login', [
   body('email', 'Enter a valid email').isEmail(),
   body('password', 'Password cannot be blank').exists()
- 
+
 ], async (req, res) => {
   const errors = validationResult(req);
-  
-  
+
+
   // Finds the validation errors in this request and wraps them in an object with handy functions
   // if there are errors, then return Bad request and the errors
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const {email,password} =req.body;
-  try{
-    let user = await User.findOne({email});
-    if(!user){
-      return res.status(400).json({error: 'Please login with correct credentials'})
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Please login with correct credentials' })
     }
     // compare the password in the database with the user entered password
     // if not found error message occured
     const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare){
-      return res.status(400).json({error: 'Please login with correct credentials'})
+    if (!passwordCompare) {
+      return res.status(400).json({ error: 'Please login with correct credentials' })
     }
     // if found, then user id will be send
-    const data={
-      user:{
+    const data = {
+      user: {
         id: user.id
       }
     }
-    const authtoken = jwt.sign(data, JWT_SECRET ); // jwt io
-    res.send({authtoken})
+    const authtoken = jwt.sign(data, JWT_SECRET); // jwt io
+    res.send({ authtoken })
   }
   catch (error) { // Fix catch block to properly handle the error
-    console.error(error.message); 
+    console.error(error.message);
     res.status(500).send("Internal Server Error");
   }
 
 })
 
-module.exports = router
+// ROUTE 3: Get loggedin user details using POST "/api/auth/getuser". Login is required
+router.post('/getuser', fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select('-password')
+    res.send(user)
+  }
+  catch (error) { // Fix catch block to properly handle the error
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
+module.exports = router 
