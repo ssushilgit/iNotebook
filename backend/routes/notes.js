ae08bf6d0da6ffc3750dd0254314f7691a4cb0a2
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-13 16:35:53 +0545

    Day 8 - Update existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,8 +4,6 @@ var fetchuser = require('../middleware/fetchuser'); // jwt nodejs
 const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator'); //import
 
-
-
 // ROUTE 1: Get all the notes using : GET "api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try{
@@ -45,6 +43,28 @@ router.post('/addnote', fetchuser, [
         console.error(error.message);
         res.status(500).send("Internal Server Error");
       }
+
+})
+
+// ROUTE 3: Update notes using : PUT "api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    // Create a newNote object
+    const newNote = {};
+    if(title) {newNote.title = title}; //if there is title, then the title will be upated at newNote
+    if(description) {newNote.description = description}; //if there is description, then the description will be upated at newNote
+    if(tag) {newNote.tag = tag}; //if there is tag, then the tag will be upated at newNote
+
+    // Find the note to be updated and update them
+    let note = await Note.findById(req.params.id) //it is the id of the user who want to update the note
+    if(!note){
+        return res.status(404).send("Not Found")
+    }
+    if(note.user.toString()!== req.user.id){ //This line checks if the user making the request is the owner of the note. It compares the ID of the authenticated user (req.user.id) with the ID of the user who created the note ('note.user'). If they do not match, it returns a 401 error response indicating that the user is not allowed to update the note.
+        return res.status(401).send("Not Allowed")
+    }
+    note =await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
 
 })
 
