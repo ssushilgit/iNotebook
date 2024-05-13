f32a129ebedc5b53422d81c7de3bf72be8ae16d7
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-13 16:59:01 +0545

    Day 8 - Delete existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,14 +6,14 @@ const { body, validationResult } = require('express-validator'); //import
 
 // ROUTE 1: Get all the notes using : GET "api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
-    try{
+    try {
         const notes = await Note.find({ user: req.user.id })
         res.json(notes)
     }
     catch (error) { // Fix catch block to properly handle the error
         console.error(error.message);
         res.status(500).send("Internal Server Error");
-      }
+    }
 })
 
 
@@ -23,9 +23,9 @@ router.post('/addnote', fetchuser, [
     body('description', 'Description must be atleast 10 characters').isLength({ min: 10 })
 
 ], async (req, res) => {
-    try{
+    try {
 
-        const {title, description, tag} = req.body;
+        const { title, description, tag } = req.body;
         // Finds the validation errors in this request and wraps them in an object with handy functions
         // if there are errors, then return Bad request and the errors
         const errors = validationResult(req);
@@ -33,39 +33,69 @@ router.post('/addnote', fetchuser, [
             return res.status(400).json({ errors: errors.array() });
         }
         const note = new Note({
-            title,description, tag, user: req.user.id
+            title, description, tag, user: req.user.id
         })
         const savedNote = await note.save()
-    
+
         res.json(savedNote)
     }
     catch (error) { // Fix catch block to properly handle the error
         console.error(error.message);
         res.status(500).send("Internal Server Error");
-      }
+    }
 
 })
 
 // ROUTE 3: Update notes using : PUT "api/notes/updatenote". Login required
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
-    const {title, description, tag} = req.body;
-    // Create a newNote object
-    const newNote = {};
-    if(title) {newNote.title = title}; //if there is title, then the title will be upated at newNote
-    if(description) {newNote.description = description}; //if there is description, then the description will be upated at newNote
-    if(tag) {newNote.tag = tag}; //if there is tag, then the tag will be upated at newNote
+    const { title, description, tag } = req.body;
+    try {
 
-    // Find the note to be updated and update them
-    let note = await Note.findById(req.params.id) //it is the id of the user who want to update the note
-    if(!note){
-        return res.status(404).send("Not Found")
-    }
-    if(note.user.toString()!== req.user.id){ //This line checks if the user making the request is the owner of the note. It compares the ID of the authenticated user (req.user.id) with the ID of the user who created the note ('note.user'). If they do not match, it returns a 401 error response indicating that the user is not allowed to update the note.
-        return res.status(401).send("Not Allowed")
-    }
-    note =await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
-    res.json({note});
+        // Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title }; //if there is title, then the title will be upated at newNote
+        if (description) { newNote.description = description }; //if there is description, then the description will be upated at newNote
+        if (tag) { newNote.tag = tag }; //if there is tag, then the tag will be upated at newNote
 
+        // Find the note to be updated and update them
+        let note = await Note.findById(req.params.id) //it is the id of the user who want to update the note
+        if (!note) {
+            return res.status(404).send("Not Found")
+        }
+        if (note.user.toString() !== req.user.id) { //This line checks if the user making the request is the owner of the note. It compares the ID of the authenticated user (req.user.id) with the ID of the user who created the note ('note.user'). If they do not match, it returns a 401 error response indicating that the user is not allowed to update the note.
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+
+    }
+})
+
+// ROUTE 4: Delete an existing note using : DELETE "api/notes/deletenote". Login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    // const { title, description, tag } = req.body;
+    try {
+        // Find the note to be delted and delete them
+        let note = await Note.findById(req.params.id) //it is the id of the user who want to update the note
+        if (!note) {
+            return res.status(404).send("Not Found")
+        }
+
+        // Allow deletion only if user owns this note
+        if (note.user.toString() !== req.user.id) { //This line checks if the user making the request is the owner of the note. It compares the ID of the authenticated user (req.user.id) with the ID of the user who created the note ('note.user'). If they do not match, it returns a 401 error response indicating that the user is not allowed to delete the note.
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted", note: note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 module.exports = router
