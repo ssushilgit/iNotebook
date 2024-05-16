a7a7d275c3ad727b280add62f24fe36aaaaf7eb8
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-16 13:51:10 +0545

    Day 11 - Fetch Notes from Context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import noteContext from '../context/notes/noteContext';
 
 const Home = () => {
+  const  context = useContext(noteContext)
+  const  {notes, setNotes} = context;
   return (
     <div>
       <div className="container my-3">
@@ -28,6 +31,10 @@ const Home = () => {
       </div >
       <div className="container my-3">
         <h1>Your Notes</h1>
+        {notes.map((note)=>{
+            return note.title;
+            // return note.description;
+        })}
         </div>
     </div>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) =>{
-    
+  
+    const notesInitial =[
+        {
+          "_id": "6641e51820bee4426a6bf3c3",
+          "user": "66405fe847b3811112b5faf8",
+          "title": "Mark Henry",
+          "description": "World's Strongest Men, WWE Superstar",
+          "tag": "wrestling",
+          "date": "2024-05-13T10:02:00.996Z",
+          "__v": 0
+        },
+        {
+          "_id": "6642f97a0ef37b2207e03ead",
+          "user": "66405fe847b3811112b5faf8",
+          "title": "Sushant KC",
+          "description": "Nepali Singer",
+          "tag": "music",
+          "date": "2024-05-14T05:41:14.584Z",
+          "__v": 0
+        },
+        {
+          "_id": "6642f9b80ef37b2207e03eb0",
+          "user": "66405fe847b3811112b5faf8",
+          "title": "Iphone 15 Pro Max",
+          "description": "Newly launched mobile phone used all around the world",
+          "tag": "mobile",
+          "date": "2024-05-14T05:42:16.101Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes, setNotes] = useState(notesInitial)
+
     return(
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
