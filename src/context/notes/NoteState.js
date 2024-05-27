0903e71bb7c9ede10e42d2702e62b81370dd14e8
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-05-28 00:46:32 +0545

    Day 16 - Adding Frontend validation

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -17,7 +17,7 @@ const NoteState = (props) => {
       }
     });
     const json = await response.json()
-    console.log(json)
+    // console.log(json)
     setNotes(json)
   }
 
@@ -34,21 +34,9 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
 
-    const json = await response.json();
-    console.log(json)
-     
-
-    console.log("Adding a new note")
-    const note = {
-      "_id": "61322f119553781a8ca8d0e08",
-      "user": "6131dc5e3e4037cd4734a0664",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2021-09-03T14:20:09.668Z",
-      "__v": 0
-    };
+    const note = await response.json();
     setNotes(notes.concat(note))
+    
   }
 
   // Delete a Note
@@ -62,9 +50,9 @@ const NoteState = (props) => {
       }
     });
     const json = response.json();
-    console.log(json)
+    // console.log(json)
 
-    console.log("Deleting the note with id" + id);
+    // console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
@@ -81,7 +69,7 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
     const json = await response.json();
-    console.log(json)
+    // console.log(json)
 
      let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
